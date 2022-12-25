da6028dc27247eb4f3df8b47dd7cc107980f530e
Andrey <Hludeev.andrey1980@gmail.com>
2022-12-25 20:34:53 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from kontroller import Controller
+from menu import Menu
+import os
+clear = lambda: os.system("cls")
+kontroller = Controller()
+
+
+menu = Menu()
+menu.AddMenuItem("Добавить домашнее животное",kontroller.add_pet)
+menu.AddMenuItem("Добавить хозяина",kontroller.add_owner)
+menu.AddMenuItem("Показать всех петов владельца",kontroller.show_all_owner_pet)
+menu.AddMenuItem("Найти всех животных по виду ",kontroller.show_pet_by_type)
+menu.AddMenuItem("Тест",kontroller.test)
+menu.AddMenuItem("Выйти из программы",exit)
+
+while True:
+    menu.Show()
+    menu.GetValueFromInput("->")()
+    input("Нажмите Enter для продолжения ")
+    clear()
+
+
+
+
+
+
+
+
+
+
+
